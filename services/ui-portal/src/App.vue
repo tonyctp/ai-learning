f48f4b1e747f1693b66dc0b341ec361c90dce7ba
YangR <YangR@WORKSHOP.HACKATHON.NET>
2022-09-29 22:02:08 +0800

    add services

---
+++ services/ui-portal/src/App.vue
@@ -0,0 +1,56 @@
+<!-- app.vue -->
+<template>
+  <div id="app">
+    <img alt="Vue logo" src="./assets/logo.png" />
+    <HelloWorld msg="Hello Vue!" />
+    <ol>
+      <todo-item
+        v-for="item in list"
+        v-bind:todo="item"
+        v-bind:key="item.id"
+      ></todo-item>
+    </ol>
+  </div>
+</template>
+
+<script>
+import HelloWorld from "./components/HelloWorld.vue";
+import TodoItem from "./components/ToDoItem.vue";
+
+export default {
+  name: "App",
+  data() {
+    return {
+      list: [
+        {
+          id: 0,
+          text: "Prepare the environment",
+        },
+        {
+          id: 1,
+          text: "Writing the code",
+        },
+        {
+          id: 2,
+          text: "Build Service",
+        },
+      ],
+    };
+  },
+  components: {
+    HelloWorld,
+    TodoItem,
+  },
+};
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
+
---
+++ services/ui-portal/src/components/ToDoItem.vue
@@ -0,0 +1,15 @@
+<!-- TodoItem.vue -->
+<template>
+  <li class="text">{{ todo.text }}</li>
+</template>
+<script>
+export default {
+  props: ["todo"],
+};
+</script>
+
+<style scoped>
+.text {
+  color: #2c3e50;
+}
+</style>
