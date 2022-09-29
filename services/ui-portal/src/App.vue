746d600a3719f260c960ccbbc6378c73585624e7
tony <tchen14@statestreet.com>
2022-09-30 05:21:43 +0800

    add chat windows

--- services/ui-portal/src/App.vue
+++ services/ui-portal/src/App.vue
@@ -1,45 +1,87 @@
 <!-- app.vue -->
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png" />
-    <HelloWorld msg="Hello Vue!" />
-    <ol>
+    <!-- <img alt="Vue logo" src="./assets/logo.png" /> -->
+    <!-- <HelloWorld msg="Auto replay chat" /> -->
+    <JwChat-index placeholder='Please input' :taleList="form.list" @enter="bindEnter" v-model="form.inputMsg" :showRightBox='true'
+      scrollType="noscroll" :config="form.config" :toolConfig ="form.toolConfig " />
+    <!-- <ol>
       <todo-item
         v-for="item in list"
         v-bind:todo="item"
         v-bind:key="item.id"
       ></todo-item>
-    </ol>
+    </ol> -->
   </div>
 </template>
 
 <script>
-import HelloWorld from "./components/HelloWorld.vue";
-import TodoItem from "./components/ToDoItem.vue";
+// import HelloWorld from "./components/HelloWorld.vue";
+// import TodoItem from "./components/ToDoItem.vue";
 
 export default {
   name: "App",
   data() {
     return {
-      list: [
-        {
-          id: 0,
-          text: "Prepare the environment",
-        },
-        {
-          id: 1,
-          text: "Writing the code",
-        },
-        {
-          id: 2,
-          text: "Build Service",
-        },
-      ],
+      form: {
+        inputMsg: 'What is exposure?',
+        list: [],
+        config: {
+          img: '../image/cover.png',
+          name: 'Finance auto replay chat',
+          dept: 'Team CRD-T Victory',
+          quickList:[{
+            text:'What is exposure?'
+          }]
+  },
+  toolConfig :{
+    active: 'win00', 
+  width: '160px',
+  listHeight: '60px', 
+  list: [ {
+    id: 'win00',
+    img: '/image/cover.png',
+    name: 'Client A',
+    dept: 'Guest'
+  },
+  {
+    id: 'win01',
+    img: '/image/three.jpeg',
+    name: 'Client B',
+    dept: 'Guest'
+  },
+  {
+    id: 'win02',
+    img: '/image/two.jpeg',
+    name: 'Client C',
+    dept: 'Guest'
+  }]
+  }
+  },
+list: [
+  {
+    id: 0,
+    text: "Prepare the environment",
+  },
+  {
+    id: 1,
+    text: "Writing the code",
+  },
+  {
+    id: 2,
+    text: "Build Service",
+  },
+],
     };
   },
-  components: {
-    HelloWorld,
-    TodoItem,
+methods: {
+  bindEnter(){
+    console.log('bindEnter')
+  }
+},
+components: {
+  // HelloWorld,
+    // TodoItem,
   },
 };
 </script>
