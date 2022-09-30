38dd1fc79fe152e9eb756c12acc2b6a2fbd37285
tony <tchen14@statestreet.com>
2022-09-30 14:40:29 +0800

    1

--- services/ui-portal/src/App.vue
+++ services/ui-portal/src/App.vue
@@ -3,8 +3,8 @@
   <div id="app">
     <!-- <img alt="Vue logo" src="./assets/logo.png" /> -->
     <!-- <HelloWorld msg="Auto replay chat" /> -->
-    <JwChat-index placeholder='Please input' :taleList="form.list" @enter="bindEnter" v-model="form.inputMsg" :showRightBox='true'
-      scrollType="noscroll" :config="form.config" :toolConfig ="form.toolConfig " />
+    <JwChat-index placeholder='Please input' :taleList="form.list" @enter="bindEnter" v-model="form.inputMsg"
+      :showRightBox='true' scrollType="noscroll" :config="form.config" :toolConfig="form.toolConfig " />
     <!-- <ol>
       <todo-item
         v-for="item in list"
@@ -25,75 +25,114 @@ export default {
     return {
       form: {
         inputMsg: 'What is exposure?',
-        list: [
-          {
-          "date": "2022/09/30 21:19:07",
-          "text": { "text": "Hi Could i ask you some finance question?" },
-          "mine": false,
-          "name": "Client A"
-        },
+        list: [     
         {
-          "date": "2022/09/30 21:19:08",
-          "text": { "text": "It's my pleasure, please." },
-          "mine": true,
-          "name": "AI bot"
-        },
-      ],
+        "date": (new Date()).toLocaleTimeString(),
+        "text": { "text": 'This auto replay bot.' },
+        "mine": false,
+        "name": "AI Bot"
+      }
+           ],
         config: {
           img: '../image/cover.png',
           name: 'Finance auto replay chat',
           dept: 'Team CRD-T Victory',
-          quickList:[{
-            text:'What is exposure?'
+          quickList: [{
+            text: 'What is exposure?'
+          },
+          {
+            text: 'What is today\'s dollar exchange rate with eur?'
+          },
+          {
+            text: 'What is yesterday\'s dollar exchange rate with eur?'
           }]
-  },
-  toolConfig :{
-    active: 'win00', 
-  width: '160px',
-  listHeight: '60px', 
-  list: [ {
-    id: 'win00',
-    img: '/image/cover.png',
-    name: 'Client A',
-    dept: 'Guest'
-  },
-  {
-    id: 'win01',
-    img: '/image/three.jpeg',
-    name: 'Client B',
-    dept: 'Guest'
-  },
-  {
-    id: 'win02',
-    img: '/image/two.jpeg',
-    name: 'Client C',
-    dept: 'Guest'
-  }]
-  }
-  },
-list: [
-  {
-    id: 0,
-    text: "Prepare the environment",
-  },
-  {
-    id: 1,
-    text: "Writing the code",
-  },
-  {
-    id: 2,
-    text: "Build Service",
-  },
-],
+        },
+        answerMock: [
+          
+        {
+          key: ['hi'],
+          answer: 'Hi, what could i help you.'
+        },
+        
+        {
+          key: ['question'],
+          answer: 'It\'s my pleasure, please.'
+        },
+        {
+          key: ['exposure'],
+          answer: 'Financial exposure is the amount an investor stands to lose in an investment should the investment fail.'
+        },
+        {
+          key: ['exchange', 'dollar', 'today'],
+          answer: 'It is 1.02 EUR to 1 USD'
+        },
+        {
+          key: ['exchange', 'dollar', 'yesterday'],
+          answer: 'It is 1.03 EUR to 1 USD.'
+        }
+        ],
+        toolConfig: {
+          active: 'win00',
+          width: '160px',
+          listHeight: '60px',
+          list: [{
+            id: 'win00',
+            img: '/image/cover.png',
+            name: 'Client A',
+            dept: 'Guest'
+          },
+          {
+            id: 'win01',
+            img: '/image/three.jpeg',
+            name: 'Client B',
+            dept: 'Guest'
+          },
+          {
+            id: 'win02',
+            img: '/image/two.jpeg',
+            name: 'Client C',
+            dept: 'Guest'
+          }]
+        }
+      },
+      list: [
+        {
+          id: 0,
+          text: "Prepare the environment",
+        },
+        {
+          id: 1,
+          text: "Writing the code",
+        },
+        {
+          id: 2,
+          text: "Build Service",
+        },
+      ],
     };
   },
-methods: {
-  bindEnter(){
-    console.log('bindEnter')
-  }
-},
-components: {
-  // HelloWorld,
+  methods: {
+    bindEnter() {
+      console.log('bindEnter')
+      const msg = this.form.inputMsg;
+      if (msg == null || msg.length == 0)
+        return;
+      var obj = this.getChatMessage(false, msg);
+      this.form.list.push(obj);
+    },
+    getChatMessage(isbot, msg) {
+      var dat = (new Date()).toLocaleTimeString();
+      const msgObj = {
+        "date": dat,
+        "text": { "text": msg },
+        "mine": true,
+        "name": "Client A"
+      }
+      return msgObj;
+    }
+  },
+  components: {
+    // HelloWorld,
     // TodoItem,
   },
 };
